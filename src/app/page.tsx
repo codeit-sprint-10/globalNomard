c4058306f3599eb9117c88794f9758665b0351b8
marchfirst01 <marchfirst3113@gmail.com>
2024-01-31 14:44:29 +0900

    feat: passwordInput 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       랜딩페이지
-      <Input title="이메일" placeholder="입력" />
+      <Input title="비밀번호" placeholder="입력" type="password" />
     </div>
   );
 }
