ba39d2a77fe7050400fdea2a7882149af548b0c4
SJvaca30 <yyvaca30@gmail.com>
2024-02-06 18:10:53 +0900

    fix: 테스트 문구 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
 'use client';
-import { Text, TextType } from '@/_styles/Text';
 
 export default function Home() {
-  return (
-    <div>
-      sdsd
-      <Text
-        $normalType={TextType.Ari31}
-        $mobileType={TextType.Sqo43}
-        text="질문을 작성하세요"
-      />
-      <Text
-        $normalType={TextType.Pre41}
-        $mobileType={TextType.Sqo43}
-        text="질문을 작성하세요"
-      />
-    </div>
-  );
+  return <div>랜딩페이지</div>;
 }
