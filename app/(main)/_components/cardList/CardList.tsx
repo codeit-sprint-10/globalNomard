ad211bda177403be40833091d0b69374d801b464
marchfirst01 <marchfirst3113@gmail.com>
2024-02-22 12:12:23 +0900

    feat: 컴포넌트 수정

--- app/(main)/_components/cardList/CardList.tsx
+++ app/(main)/_components/cardList/CardList.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import Card from '../card/Card';
-import { Activity } from '../type';
+import { Activities } from '../type';
 import * as S from './cardList.style';
 import ArrowLeft from '@/assets/icons/Arrow_left.svg';
 import ArrowRight from '@/assets/icons/Arrow_right.svg';
 import Text, { TextType } from '@/_styles/Text';
 
-export default function CardList({ data }: Activity) {
+export default function CardList({ data }: Activities) {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   const handleLeftButtonClick = () => {
--- app/(main)/_components/type.ts
+++ app/(main)/_components/type.ts
@@ -1,5 +1,6 @@
 import { StaticImageData } from 'next/image';
 
+// activityDummyType ~
 export interface Data {
   id: number;
   images: StaticImageData;
@@ -8,6 +9,20 @@ export interface Data {
   point: string;
 }
 
-export interface Activity {
+export interface Act {
   data: Data[];
+}
+// ~ activityDummyType
+
+export interface Activity {
+  id: number;
+  images: StaticImageData;
+  title: string;
+  price: string;
+  rating: number;
+  reviewCount: number;
+}
+
+export interface Activities {
+  data: Activity[];
 }
