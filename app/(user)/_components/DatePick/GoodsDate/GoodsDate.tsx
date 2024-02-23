f407bdd6553c0f4bab44eb9d67b3cf28e4219daa
Tae hee <lte1807@gmail.com>
2024-02-23 15:08:18 +0900

    feat: 체험 상세 페이지

---
+++ app/(user)/_components/DatePick/GoodsDate/GoodsDate.tsx
@@ -0,0 +1,183 @@
+import Add from '@/assets/icons/Add';
+import Subtract from '@/assets/icons/Subtract';
+import { Schedule } from '@/_api/goods/goods.types';
+import { PlainButton } from '@/_components/Button/PlainButton/PlainButton';
+import COLORS from '@/_styles/colors';
+import Text, { TextType } from '@/_styles/Text';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { styled } from 'styled-components';
+
+interface props {
+  start: Schedule;
+  end: Schedule;
+}
+
+function GoodsDate({ start, end }: props) {
+  const [headCount, setHeadCount] = useState(1);
+
+  // const [startDate, setStartDate] = useState(new Date());
+  // const [endDate, setEndDate] = useState(null);
+  // const onChange = (dates: any) => {
+  //   const [start, end] = dates;
+  //   setStartDate(start);
+  //   setEndDate(end);
+  // };
+
+  const AddCount = () => {
+    setHeadCount(headCount + 1);
+  };
+
+  const SubtractCount = () => {
+    if (headCount > 1) {
+      setHeadCount(headCount - 1);
+    }
+  };
+  return (
+    <DatePickContainer>
+      <PerToPerson>
+        <Text $normalType={TextType.Pre19} text="₩ 1,000 " />/ 인
+      </PerToPerson>
+      <Calender>
+        <Text $normalType={TextType.Sqo42} text="날짜" />
+        {/* <StyledDatePicker selected={startDate} onChange={onChange} inline /> */}
+      </Calender>
+      <Reservation>
+        <Text $normalType={TextType.Pre25} text="예약 가능한 시간" />
+        <ReservationTime>
+          <StartDate>{`${start.startTime} ~ ${start.endTime}`}</StartDate>
+          <EndDate>{`${end.startTime} ~ ${end.endTime}`}</EndDate>
+        </ReservationTime>
+      </Reservation>
+      <NumberOfPeople>
+        <Text $normalType={TextType.Pre9} text="참여 인원 수" />
+        <NumberButton>
+          <button onClick={AddCount}>
+            <Add />
+          </button>
+          <NumberText>{headCount}</NumberText>
+          <button onClick={SubtractCount}>
+            <Subtract />
+          </button>
+        </NumberButton>
+        <PlainButton style="primary" roundSize="S" height="56px">
+          <Text $normalType={TextType.Pre10} text="예약하기" />
+        </PlainButton>
+        <TotalCount>
+          <TotalText>총 합계</TotalText>
+          <TotalPrice>{`₩ ${headCount * 1000}`}</TotalPrice>
+        </TotalCount>
+      </NumberOfPeople>
+    </DatePickContainer>
+  );
+}
+
+export default GoodsDate;
+
+const DatePickContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 38.4rem;
+  border: 1px solid ${COLORS.GRAY30};
+  border-radius: 1rem;
+  padding: 2.4rem;
+  overflow: hidden;
+  gap: 1.6rem;
+`;
+
+const PerToPerson = styled.div`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+`;
+
+const Calender = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid ${COLORS.GRAY30};
+  padding-top: 1.6rem;
+  gap: 1.6rem;
+`;
+
+const StyledDatePicker = styled(DatePicker)`
+  background-color: transparent;
+  color: #707070;
+  font-size: 40px;
+`;
+
+const ReservationTime = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+const StartDate = styled.div`
+  display: flex;
+  padding: 10px 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 8px;
+  background-color: ${COLORS.BLACK50};
+  color: ${COLORS.WHITE10};
+`;
+
+const EndDate = styled.div`
+  display: flex;
+  padding: 10px 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border: 1px solid ${COLORS.BLACK50};
+  border-radius: 8px;
+`;
+
+const Reservation = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const NumberOfPeople = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 12px;
+  border-top: 1px solid ${COLORS.GRAY30};
+  gap: 2.4rem;
+`;
+
+const NumberButton = styled.div`
+  width: 120px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid ${COLORS.GRAY30};
+  border-radius: 0.6rem;
+  border-radius: 6px;
+  padding: 10px;
+`;
+
+const NumberText = styled.div`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+`;
+
+const TotalCount = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 1.6rem;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 26px;
+  border-top: 1px solid ${COLORS.GRAY30};
+`;
+
+const TotalText = styled.div``;
+
+const TotalPrice = styled.div``;
--- app/(user)/goods/[id]/page.tsx
+++ app/(user)/goods/[id]/page.tsx
@@ -1,7 +1,103 @@
-import React from 'react';
+'use client';
+import { TopLevel } from '@/_api/goods/goods.types';
+import goodsList from '@/(user)/goods/[id]/mock.json';
+import Image from 'next/image';
+import { styled } from 'styled-components';
+import COLORS from '@/_styles/colors';
+import Meatball from '@/assets/icons/Meatball';
+import Text, { TextType } from '@/_styles/Text';
+import GoodsDate from '@/(user)/_components/DatePick/GoodsDate/GoodsDate';
 
-function Page() {
-  return <div>체험 상세 페이지</div>;
+function Page({ params }: { params: { id: string } }) {
+  const Activities: TopLevel = goodsList;
+
+  // async function getActivities(userId: string) {
+  //   const res = await fetch(
+  //     `https://sp-globalnomad-api.vercel.app/01-10/activities/${userId}`,
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     },
+  //   );
+  //   return res.json();
+  //
+
+  return (
+    <Wrapper>
+      <Container>
+        <Category>{Activities.category}</Category>
+        <TitleStyle>
+          {Activities.title}
+          <Meatball />
+        </TitleStyle>
+
+        <Banner
+          src={Activities.bannerImageUrl}
+          width={100}
+          height={100}
+          alt="배너 이미지"
+        />
+
+        <DescriptionSection>
+          <Text $normalType={TextType.Pre14} text="체험 설명" />
+          <Description>{Activities.description}</Description>
+        </DescriptionSection>
+        <GoodsDate
+          start={Activities.schedules[0]}
+          end={Activities.schedules[1]}
+        />
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default Page;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled.div``;
+const TitleStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin-bottom: 4.3rem;
+`;
+
+const Category = styled.div`
+  color: ${COLORS.GRAY70};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  margin-bottom: 1rem;
+`;
+
+const Banner = styled(Image)`
+  width: 100%;
+  height: 53.4rem;
+  margin-bottom: 8.6rem;
+`;
+
+const DescriptionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  border-top: 1px solid black;
+  width: 79rem;
+  padding: 40px 0;
+  margin-right: 2.4rem;
+  float: left;
+`;
+const Description = styled.div`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  color: ${COLORS.GRAY70};
+`;
