0654160ed33d4f886ece2b3e4d7e8891a49d5929
SJvaca30 <yyvaca30@gmail.com>
2024-02-28 18:05:48 +0900

    feat: GNBNav API

--- app/_api/postUser.ts
+++ app/_api/postUser.ts
@@ -18,12 +18,13 @@ export const postUser = async ({ email, password }: Props) => {
     password,
   };
 
-  console.log(body, 'postUser 테스트');
+  console.log(body, 'postUser body 테스트');
 
   const { data } = await fetcher<UserRes, Props>({
     url: '/auth/login',
     method: 'POST',
     data: body,
   });
+  console.log(data, 'postUser data 테스트');
   return data;
 };
